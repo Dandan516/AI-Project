ad65650b53e29efe2cafdbe99cae8cd9cffb1c9e
Dandan516 <dennisfung0516@gmail.com>
2024-09-19 05:20:18 +0800

    n

--- interface.py
+++ interface.py
@@ -6,16 +6,16 @@ from product_suggestion import find_products
 def show_outputs():
     return gr.update(visible=True)
 
-def get_result(inp):
-    time.sleep(1)
-    return find_products(inp)
+def get_result(input):
+    time.sleep(0.1)
+    return find_products(input)
 
 with gr.Blocks() as interface:
     gr.Markdown("Product suggestion AI")
-    inp = gr.Textbox(placeholder="I want to go hiking.")
+    user_input = gr.Textbox(placeholder="I want to go hiking.")
     btn = gr.Button(value="Run")
     result = gr.Dataframe(visible=False, interactive=False)
     btn.click(fn=show_outputs, outputs=result)
-    btn.click(fn=get_result, inputs=inp, outputs=result)
+    btn.click(fn=get_result, inputs=user_input, outputs=result)
 
 interface.launch()
